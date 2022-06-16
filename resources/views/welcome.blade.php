eb88623fa0d3576b6265d975cd1cef8eb69374e7
Pollygan <90186074+Pollygan@users.noreply.github.com>
2022-06-16 20:46:33 +0200

    styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,7 +46,6 @@
                         </g>
                     </svg>
                 </div>
-                <a href="{{ route('products.index') }}" class="btn btn-primary">Test here!</a>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
@@ -56,6 +55,8 @@
                                 <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Documentation</a></div>
                             </div>
 
+                            <a href="{{ route('products.index') }}" class="btn btn-primary">Test here!</a>
+                            
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
                                     Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
