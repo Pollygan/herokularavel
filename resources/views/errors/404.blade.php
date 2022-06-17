f7cc12b9f52ba614f2b02cd327bb3e1512771ac1
Pollygan <90186074+Pollygan@users.noreply.github.com>
2022-06-17 15:50:50 +0200

    Adding 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+
+@section('content')
+
+    <section style="padding-top: 100px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 offset-md-2 text-center">
+                    <h1 style="font-size: 162px; ">404</h1> 
+                    <h2>Page Not Found</h2>
+                    <p>We are sorry,the page you requested could not be found. Please go to the home page.</p>
+                    <a href="/" class="btn btn-primary">Visit Homepage</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+
+@endsection
+
+
