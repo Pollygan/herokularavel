b82d8f5eb4a50127e34234c1b917ec85d06ae27e
Pollygan <90186074+Pollygan@users.noreply.github.com>
2022-06-18 16:43:35 +0200

    Test validation

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -13,6 +13,16 @@
         <form action="{{ route('products.store') }}" method="POST">
             @csrf
 
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
