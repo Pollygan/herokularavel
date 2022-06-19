84fbbf84328d108819bd6a24f76fc232ad39cf84
Pollygan <90186074+Pollygan@users.noreply.github.com>
2022-06-19 12:16:21 +0200

    Test validation if statment

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -18,13 +18,18 @@
                     <div class="form-group">
                         <strong>Name</strong>
                         <label>
-                            <input type="text" name="name" class="form-control" placeholder="Fill in the name of the product here">
+                            <input type="text" name="name" class="form-control" placeholder="Fill in the name here">
                         </label>
                     </div>
                 </div>
 
-                <br>
-                <br>
+                @if($errors->any()) {
+
+                   <input type="text" name="name" class="input is-success" placeholder="Good">
+                } @else {
+                  <input type="text" name="name" class="input is-danger" placeholder="The field is required">
+                }
+                @endif
 
                 <div class="col-xs-12 col-sm-12 col-md-12 pt-5">
                     <div class="form-group">
