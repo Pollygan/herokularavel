d81bb0a2a21485b1743755def69195b8911d31bf
Pollygan <90186074+Pollygan@users.noreply.github.com>
2022-06-19 14:28:45 +0200

    Test

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -10,17 +10,6 @@
             </div>
         </div>
 
-
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-
         <form id="demoForm" action="{{ route('products.store') }}" method="POST">
             @csrf
 
